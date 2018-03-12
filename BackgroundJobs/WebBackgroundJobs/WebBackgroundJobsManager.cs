8ef12461fbfc4b9f476eb33a67823c99397d203f
yury deshin <j.deshin@hotmail.com>
2018-03-12 18:34:36 +0300

    initial

---
+++ BackgroundJobs/WebBackgroundJobs/WebBackgroundJobsManager.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ScriptEngine.Machine;
+using ScriptEngine.Machine.Contexts;
+using ScriptEngine.HostedScript.Library;
+
+namespace OneScript.HttpServices
+{
+    public static class WebBackgroundJobsManager
+    {
+        static System.Collections.Hashtable jobsKeys = new System.Collections.Hashtable();
+        static System.Collections.Hashtable jobs = new System.Collections.Hashtable();
+
+        public static void ExecuteJob(object stateInfo)
+        {
+            WebBackgroundJob job = (WebBackgroundJob)stateInfo;
+
+            System.Collections.Hashtable syncJobsKeys;
+            syncJobsKeys = System.Collections.Hashtable.Synchronized(jobsKeys);
+
+            if (job.Key != "" && job.Key != null)
+            {
+                // Пробуем вставить в таблицу ключей
+                //, если вставка неудачна, значит фоновое задание уже выполняется
+
+                try
+                {
+                    syncJobsKeys.Add(job.Key, job.Key);
+                }
+                catch (ArgumentException)
+                {
+                    // Такое значение уже есть в списке, не запускаем задание
+                    return;
+                }
+            }
+
+            // Заполняем значения работы и вставляем ее в список
+            System.Collections.Hashtable syncJobs = System.Collections.Hashtable.Synchronized(jobs);
+            syncJobs.Add(job.UUID, job);
+            job.Begin = DateTime.Now;
+
+            try
+            {
+                AspNetHostEngine engine = new AspNetHostEngine();
+                engine.CallCommonModuleProcedure(job.MethodName, job.ExecutionParameters);
+                job.State = BackgroundJobState.Completed;
+                job.ExecutionParameters = null;
+            }
+            catch (ScriptEngine.ScriptException ex)
+            {
+                job.ErrorInfo = new ExceptionInfoContext(ex);
+                job.State = BackgroundJobState.Failed;
+
+                System.IO.TextWriter logWriter = AspNetLog.Open();
+                AspNetLog.Write(logWriter, "Error executing background job ");
+                AspNetLog.Write(logWriter, ex.ToString());
+                AspNetLog.Close(logWriter);
+            }
+            catch (Exception ex)
+            {
+                job.State = BackgroundJobState.Failed;
+
+                System.IO.TextWriter logWriter = AspNetLog.Open();
+                AspNetLog.Write(logWriter, "Error executing background job ");
+                AspNetLog.Write(logWriter, ex.ToString());
+                AspNetLog.Close(logWriter);
+            }
+            finally
+            {
+                job.End = DateTime.Now;
+
+                try
+                {
+                    syncJobs.Remove(job.UUID);
+                }
+                catch {  /* Ничего не делаем*/}
+
+                try
+                {
+                    if (job.Key != null)
+                        syncJobsKeys.Remove(job.Key);
+                }
+                catch {  /* Ничего не делаем*/}
+            }
+        }
+    }
+}
---
+++ BackgroundJobs/WebBackgroundJobs/WebBackgroundJobsManagerImpl.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+using ScriptEngine.Machine;
+using ScriptEngine.Machine.Contexts;
+using ScriptEngine.HostedScript.Library;
+
+
+namespace OneScript.HttpServices
+{
+    [ContextClass("МенеджерФоновыхЗаданийWeb", "WebBackgroundJobsManager")]
+    public class WebBackgroundJobsManagerImpl : AutoContext<WebBackgroundJobsManagerImpl>
+    {
+        [ScriptConstructor(Name = "Без параметров")]
+        public static IRuntimeContextInstance Constructor()
+        {
+            return new WebBackgroundJobsManagerImpl();
+        }
+
+        [ContextMethod("Выполнить", "Execute")]
+        public WebBackgroundJobImpl Execute(string methodName, ArrayImpl parameters, string key = "", string description = "")
+        {
+            WebBackgroundJob job = new WebBackgroundJob();
+            WebBackgroundJobImpl jobImpl = new WebBackgroundJobImpl(job);
+
+            if (key != null)
+                job.Key = key;
+
+            if (description != null)
+                job.Description = description;
+
+            job.ExecutionParameters = new IValue[0];
+
+            if (parameters != null)
+            {
+                job.ExecutionParameters = new IValue[parameters.Count()];
+                int index = 0;
+
+                foreach (IValue cv in parameters)
+                {
+                    job.ExecutionParameters[index] = cv;
+                    index++;
+                }
+            }
+
+            job.MethodName = methodName;
+
+            ThreadPool.QueueUserWorkItem(new WaitCallback(WebBackgroundJobsManager.ExecuteJob), job);
+
+            return jobImpl;
+        }
+
+        [ContextMethod("ОжидатьЗавершения", "WaitForCompletion")]
+        public void WaitForCompletion(ArrayImpl backgroundJobs, int? timeout = null)
+        {
+            int delta = 1;
+
+            if (timeout == null)
+            {
+                delta = 0;
+                timeout = 1;
+            }
+
+            int current = 0;
+            WebBackgroundJobImpl failedJob = null;
+            WebBackgroundJobImpl notCompletedJob = null;
+
+            do
+            {
+                System.Threading.Thread.Sleep(1000);
+                current += delta;
+
+                notCompletedJob = null;
+
+                foreach (IValue cj in backgroundJobs)
+                {
+                    if (((WebBackgroundJobImpl)cj).State == WebBackgroundJobStateImpl.Active)
+                        notCompletedJob = (WebBackgroundJobImpl)cj;
+                    if (((WebBackgroundJobImpl)cj).State == WebBackgroundJobStateImpl.Failed)
+                    {
+                        failedJob = (WebBackgroundJobImpl)cj;
+                        break;
+                    }
+                }
+
+            } while (current < timeout && notCompletedJob != null && failedJob == null);
+
+            System.IO.TextWriter logWriter;
+
+            if (failedJob != null)
+            {
+                logWriter = AspNetLog.Open();
+                string logStr = failedJob.ErrorInfo.ModuleName + "`n"
+                    + failedJob.ErrorInfo.LineNumber + "`n"
+                    + failedJob.ErrorInfo.Description + "`n"
+                    + failedJob.ErrorInfo.DetailedDescription;
+                AspNetLog.Write(logWriter, logStr);
+                AspNetLog.Close(logWriter);
+
+                throw (new Exception("Одно или несколько фоновых заданий завершились с ошибкой."));
+            }
+
+            if (notCompletedJob == null)
+                return;
+
+            string exceptionString = "Timeout expires for job: ";
+
+            exceptionString += "start date: " + notCompletedJob.Begin.ToString() + " ";
+            exceptionString += "method: " + notCompletedJob.MethodName + " ";
+
+            if (notCompletedJob.Description != null)
+                exceptionString += "description: " + notCompletedJob.Description + " ";
+            if (notCompletedJob.Key != null)
+                exceptionString += "key: " + notCompletedJob.Description + " ";
+
+            logWriter = AspNetLog.Open();
+            AspNetLog.Write(logWriter, exceptionString);
+            AspNetLog.Close(logWriter);
+
+            throw (new Exception(exceptionString));
+        }
+
+    }
+}
