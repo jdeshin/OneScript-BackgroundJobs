0a44226f78840b4cf146e031c8bf4c9aa8cb19f6
yury deshin <j.deshin@hotmail.com>
2018-03-20 10:04:32 +0300

    Добавил версию

--- BackgroundJobs/WebBackgroundJobs/Properties/AssemblyInfo.cs
+++ BackgroundJobs/WebBackgroundJobs/Properties/AssemblyInfo.cs
@@ -33,6 +33,4 @@ using System.Runtime.InteropServices;
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.*")]
-#pragma warning disable CS7035 // The specified version string does not conform to the recommended format - major.minor.build.revision
-[assembly: AssemblyFileVersion(version: "1.0.*")]
-#pragma warning restore CS7035 // The specified version string does not conform to the recommended format - major.minor.build.revision
+//[assembly: AssemblyFileVersion("1.0.0.0")]
